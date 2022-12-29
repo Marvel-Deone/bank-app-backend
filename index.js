975e2c33ce5aaf8793d7f6f2e6d92c6622579613
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2022-12-29 14:51:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect(URI, (err)=> {
 mongoose.Promise = global.Promise;
 
 app.get('/', (req,res) =>{
-    res.json("Kelvin is a good boy")
+    res.json("Fola is a good girl")
 })
 app.use('/api', routes)
 
