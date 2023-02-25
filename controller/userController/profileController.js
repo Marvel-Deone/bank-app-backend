e4ecffe426cb04cb4c413e40b9c9a441ace0ed64
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2023-02-25 18:28:49 +0100

    Added new update

--- controller/userController/profileController.js
+++ controller/userController/profileController.js
@@ -44,9 +44,7 @@ const sendOtp = async (req, res) => {
         token: otp,
       }).save();
     }
-    // await sendMail({ email, otp: token.token, subject: "Bank-app email verification otp" });
-    sendMail({ email, subject: "Bank-app email verification otp" }).then(resp => res.send( res.json({ message: resp.msg, success: true })))
-    .catch(err => res.status(500).send(err.msg))
+    await sendMail({ email, subject: "Bank-app email verification otp" });
     // res.json({ message: "OTP sent to your mail", success: true });
   } catch (error) {
     res.json({ message: `Failed to send otp`, success: false });
